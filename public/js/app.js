e07ac82ce3750621110e8177382a19c652d7ac68
Teddy Perera <pereratdp@gmail.com>
2020-07-18 21:40:56 +1000

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('clientside javascrip is loaded')
-
 const weatherForm = document.querySelector('form')
 const address = document.querySelector('input')
 const messageData = document.querySelector('#data')
